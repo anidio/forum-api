28eb6b829c6586f85e555a0dff0daae986aab097
Isaias Roberto <isaias.reng@gmail.com>
2026-05-29 17:57:05 -0300

    feat:₢reate database module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AuthModule } from './auth/auth.module';
 import { UserService } from './user/user.service';
 import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, DatabaseModule],
   controllers: [UserController],
   providers: [UserService],
 })
